406531962b28a8ee21608d31c71690e9765360ba
Tim Preece <tdpreece@gmail.com>
2015-06-25 14:02:19 +0100

    Added variable for start page url to test case.

--- functional_tests.py
+++ functional_tests.py
@@ -3,8 +3,10 @@ import unittest
 from selenium import webdriver
 
 class InitialPageLoadTests(unittest.TestCase):
+    START_PAGE_URL = 'file:///home/tdpreece/javascript_projects/pacman_javascript_qunit_requirejs/index.html'
+
     def test_title_of_page_is_pacman(self):
         browser = webdriver.PhantomJS()
-        browser.get('file:///home/tdpreece/javascript_projects/pacman_javascript_qunit_requirejs/index.html')
+        browser.get(self.START_PAGE_URL)
         self.assertIn('PacMan', browser.title)
 
