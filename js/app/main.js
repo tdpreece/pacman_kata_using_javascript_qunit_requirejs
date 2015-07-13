284051b39e8e57d9264675c28cfc568dcc387986
Tim Preece <tdpreece@gmail.com>
2015-07-13 19:14:36 +0100

    Fix missing document selector before ready.

--- js/app/main.js
+++ js/app/main.js
@@ -1,6 +1,6 @@
 define(['jquery'], function (jquery) {
     console.log("app/main loaded OK");
-    jquery.ready(function () {
+    jquery(document).ready(function () {
         jquery('body').append("<div id='pacman'>V</div>");
     });
 });
