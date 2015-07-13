3c447d1d7d709765b46f693431ce8c24749739f3
Tim Preece <tdpreece@gmail.com>
2015-07-13 19:03:16 +0100

    Moved instantiation of browser into setUp.

--- functional_tests.py
+++ functional_tests.py
@@ -6,8 +6,11 @@ from selenium.webdriver.common.by import By
 class InitialPageLoadTests(unittest.TestCase):
     START_PAGE_URL = 'file:///home/tdpreece/javascript_projects/pacman_javascript_qunit_requirejs/index.html'
 
-    def assert_element_with_id_exists(self, browser, id):
-        elements = browser.find_elements(By.ID, id)
+    def setUp(self):
+        self.browser = webdriver.PhantomJS()
+
+    def assert_element_with_id_exists(self, id):
+        elements = self.browser.find_elements(By.ID, id)
         number_of_elements = len(elements)
         self.assertEqual(
             number_of_elements,
@@ -17,11 +20,10 @@ class InitialPageLoadTests(unittest.TestCase):
         )
 
     def test_title_of_page_is_pacman(self):
-        browser = webdriver.PhantomJS()
-        browser.get(self.START_PAGE_URL)
-        self.assertIn('PacMan', browser.title)
+        self.browser.get(self.START_PAGE_URL)
+        self.assertIn('PacMan', self.browser.title)
 
     def test_pacman_is_on_page(self):
-        browser = webdriver.PhantomJS()
-        browser.get(self.START_PAGE_URL)
-        self.assert_element_with_id_exists(browser, 'pacman')
+        self.browser = webdriver.PhantomJS()
+        self.browser.get(self.START_PAGE_URL)
+        self.assert_element_with_id_exists('pacman')
